d5b021eb0a0e9e374d11d8ddb738d9a05c4df585
Martzka <maritzka544@gmail.com>
2023-11-24 17:11:32 +0800

    first commit

--- dashboard.php
+++ dashboard.php
@@ -1,168 +1,181 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Welcome</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Welcome</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="dashboard.css">
   <style>
-     body {
-        background-image: url('https://source.unsplash.com/1200x250/?school');
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-attachment: fixed;
+    body {
+      background-image: url('https://i.pinimg.com/736x/f0/eb/b0/f0ebb045849748b3c545a7a77493ba7e.jpg');
+      background-repeat: no-repeat;
+      background-size: cover;
+      background-attachment: fixed;
     }
   </style>
 
 
-  </head>
-  <body>
-    <div class="container">
-        <div class="row d-flex align-items-center justify-content-center" style="height: 100vh; width:100%">
-            <div class="col-md-8">
-                <div class="card border-0 shadow rounded">
-                  <div class="card-header mb-3">
-                    <strong>Aplikasi PeSaT - Pencatatan Siswa Terlambat</strong>
-                  </div>
-                  <form name="kirim_data">
-                    <div class="card-body">
-                      <!-- alert -->
-                      <div class="alert alert-success alert-dismissible fade show alert-berhasil d-none" role="alert">
-                        <strong>Kirim data berhasil!</strong> Data sudah masuk ke dalam database aplikasi Pesat..
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                      </div>
-                      <button type="button" class="btn btn-danger float-right" onclick="logout()">Logout</button>
-                      <button type="button" class="btn btn-primary float-right" id="tambahBaris">Tambah Data</button>
-                      <table class="table" id="tabelData">
-                        <thead>
-                            <tr>
-                                <th>Jam Terlambat</th>
-                                <th>Nama Siswa</th>
-                                <th>Kelas</th>
-                                <th>Keterangan</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody id="tabelBody">
-                        <tr>
-                          <td>
-                            <input type="datetime-local" class="form-control" id="birthdaytime" name="jam_terlambat">
-                          </td>
-                            <td>
-                              <select class="form-control pilihan" name="kelas">
-                                <option value="">Pilih Kelas</option>
-                                <option value="X PPLG">X PPLG</option>
-                                <option value="X DKV">X DKV</option>
-                                <option value="X TJKT 1">X TJKT 1</option>
-                                <option value="X TJKT 2">X TJKT 2</option>
-                              </select>
-                            </td>
-                            <td>                   
-                              <input type="text" name="nama" class="form-control" placeholder="Nama Siswa">
-                            </td>
-                            <td>
-                              <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
-                                <input type="checkbox" class="btn-check" name="status" value="Terlambat" id="btncheck1" autocomplete="off" checked>
-                                <label class="btn btn-outline-primary" for="btncheck1">Terlambat</label>
-                              </div>
-                            </td>
-                            <td>
-                              <a href="" class="btn btn-danger btn-remove"><span class="badge rounded-pill text-bg-danger"><i class="bi bi-trash-fill"></i></span></a>
-                            </td>
-                        </tr>
-                            <!-- Tambahkan baris sebanyak yang Anda perlukan -->
-                        </tbody>
-                      </table>
-                    </div>
-                    <div class="p-3">
-                        <button type="submit" class="btn btn-success btn-kirim">Kirim data</button>
-                        <button class="btn btn-success btn-loading d-none" type="button" disabled>
-                        <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
-                        <span role="status">Loading...</span>
-                      </button>
-                    </div>
-                  </form>
-            </div>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row d-flex align-items-center justify-content-center" style="height: 100vh; width:100%">
+      <div class="col-md-8">
+        <div class="card border-0 shadow rounded">
+          <div class="card-header mb-3">
+            <strong>Aplikasi Ibadah - Pencatatan 11 RPL Sholat</strong>
           </div>
+          <form name="kirim_data">
+            <div class="card-body">
+              <!-- alert -->
+              <div class="alert alert-success alert-dismissible fade show alert-berhasil d-none" role="alert">
+                <strong>Kirim data berhasil!</strong> Data sudah masuk ke dalam database aplikasi Pesat..
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+              </div>
+              <button type="button" class="btn btn-danger float-right" onclick="logout()">Logout</button>
+              <button type="button" class="btn btn-primary float-right" id="tambahBaris">Tambah Data</button>
+              <table class="table" id="tabelData">
+                <thead>
+                  <tr>
+                    <th>Tanggal Sholat</th>
+                    <th>Sholat</th>
+                    <th>Nama Siswa</th>
+                    <th>Keterangan</th>
+                    <th>Aksi</th>
+                  </tr>
+                </thead>
+                <tbody id="tabelBody">
+                  <tr>
+                    <td>
+                      <input type="datetime-local" class="form-control" id="birthdaytime" name="Tanggal sholat">
+                    </td>
+                    <td>
+                      <select class="form-control pilihan" name="Sholat">
+                        <option value="">Pilih</option>
+                        <option value="Sholat Zuhur">Sholat Zuhur</option>
+                        <option value="Sholat Ashar">Sholat Ashar</option>
+                        <option value="Tidak Sholat">Tidak Sholat</option>
+                      </select>
+                    </td>
+                    <td>
+                      <input type="text" name="nama" class="form-control" placeholder="Nama Siswa">
+                    </td>
+                    <td>
+                      <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
+                        <input type="checkbox" class="btn-check" name="status" value="Terlambat" id="btncheck1"
+                          autocomplete="off" checked>
+                    <td>
+                      <input id="keterangan-sholat" type="text" name="keterangan" class="form-control"
+                        placeholder="Keterangan">
+                    </td>
+            </div>
+            </td>
+            <td>
+              <a href="" id="remove-button" class="btn btn-danger btn-remove"><span
+                  class="badge rounded-pill text-bg-danger"><i class="bi bi-trash-fill"></i></span></a>
+            </td>
+            </tr>
+            <!-- Tambahkan baris sebanyak yang Anda perlukan -->
+            </tbody>
+            </table>
         </div>
+        <div class="p-3">
+          <button type="submit" class="btn btn-success btn-kirim">Kirim data</button>
+          <button class="btn btn-success btn-loading d-none" type="button" disabled>
+            <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
+            <span role="status">Loading...</span>
+          </button>
+        </div>
+        </form>
+      </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+    crossorigin="anonymous"></script>
 
-    <!-- proses kirim data ke database -->
-    <script>
-      const scriptURL = 'https://script.google.com/macros/s/AKfycbz0rT6lIQllbDFc5PP9nxkELh1PvGpxY5iW0oS-bHbaAg799NAwz7WidCJw-S2TezT5/exec'
-      const form = document.forms['kirim_data']
-      const btnLoading = document.querySelector('.btn-loading');
-      const btnKirim = document.querySelector('.btn-kirim');
-      const alertBerhasil = document.querySelector('.alert-berhasil');
+  <!-- proses kirim data ke database -->
+  <script>
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbyfr_8J_NevlVGX3xTqr1xX0neqkrFwKYj3rKqx3d02T6uHvHnU_MjMVtRDy5P53by_/exec'
+    const form = document.forms['kirim_data']
+    const btnLoading = document.querySelector('.btn-loading');
+    const btnKirim = document.querySelector('.btn-kirim');
+    const alertBerhasil = document.querySelector('.alert-berhasil');
 
-      form.addEventListener('submit', e => {
-        e.preventDefault();
+    form.addEventListener('submit', e => {
+      e.preventDefault();
 
-        btnLoading.classList.toggle('d-none');
-        btnKirim.classList.toggle('d-none');
+      btnLoading.classList.toggle('d-none');
+      btnKirim.classList.toggle('d-none');
 
-        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-          .then(response => {
-            btnLoading.classList.toggle('d-none');
-            btnKirim.classList.toggle('d-none');
+      fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+          btnLoading.classList.toggle('d-none');
+          btnKirim.classList.toggle('d-none');
 
-            alertBerhasil.classList.toggle('d-none');
-            form.reset();
+          alertBerhasil.classList.toggle('d-none');
+          form.reset();
 
-            console.log('Success!', response);
-          })
-          .catch(error => console.error('Error!', error.message))
-      });
-    </script>
+          console.log('Success!', response);
+        })
+        .catch(error => console.error('Error!', error.message))
+    });
+  </script>
 
-    <script>
-        function logout() {
-            console.log("Berhasil logout");
-            window.location.href = "index.php";
-            history.pushState(null, null, "index.php");
-        }
-    </script>
+  <script>
+    function logout() {
+      console.log("Berhasil logout");
+      window.location.href = "index.php";
+      history.pushState(null, null, "index.php");
+    }
+  </script>
 
-    <script>
-      document.getElementById('tambahBaris').addEventListener('click', function() {
-        var tbody = document.getElementById('tabelBody');
-        var newRow = document.createElement('tr');
-        var rowCount = tbody.getElementsByTagName('tr').length + 1; // Menghitung jumlah baris
-    
-        newRow.innerHTML = `
+  <script>
+    document.getElementById('tambahBaris').addEventListener('click', function () {
+      var tbody = document.getElementById('tabelBody');
+      var newRow = document.createElement('tr');
+      var rowCount = tbody.getElementsByTagName('tr').length + 1; // Menghitung jumlah baris
+
+      newRow.innerHTML = `
         <td>                   
           <input type="datetime-local" class="form-control" id="birthdaytime" name="jam_terlambat_${rowCount}">
         <td>
-          <select class="form-control" id="pilihan" name="kelas_${rowCount}">
-            <option value="">Pilih Kelas</option>
-            <option value="X PPLG">X PPLG</option>
-            <option value="X DKV">X DKV</option>
-            <option value="X TJKT 1">X TJKT 1</option>
-            <option value="X TJKT 2">X TJKT 2</option>
+          <select class="form-control" id="pilihan" name="Sholat_${rowCount}">
+          <option value="">Pilih</option>
+          <option value="Sholat Zuhur">Sholat Zuhur</option>
+          <option value="Sholat Ashar">Sholat Ashar</option>
+          <option value="Tidak Sholat">Tidak Sholat</option>
           </select>
         <td>
             <input type="text" class="form-control" name="nama_${rowCount}" placeholder="Nama Siswa">
         </td>
         <td>
-            <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
-                <input type="checkbox" class="btn-check" autocomplete="off" checked>
-                <label class="btn btn-outline-primary">Terlambat</label>
-            </div>
+        <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
+               <input type="checkbox" class="btn-check" name="status" value="Terlambat" id="btncheck1" autocomplete="off" checked>
+                 <td>                   
+                    <input id="keterangan-sholat" type="text" name="keterangan" class="form-control" placeholder="Keterangan">
+                 </td>
+          </div>
         </td>
         <td>
-          <a href="" class="btn btn-danger btn-remove"><span class="badge rounded-pill text-bg-danger"><i class="bi bi-trash-fill"></i></span></a>
-        </td>
+              <a href="" id="remove-button" class="btn btn-danger btn-remove"><span
+                  class="badge rounded-pill text-bg-danger"><i class="bi bi-trash-fill"></i></span></a>
+            </td>
         `;
-    
-        tbody.appendChild(newRow);
+
+      tbody.appendChild(newRow);
     });
-    document.getElementById('tabelBody').addEventListener('click', function(e) {
+    document.getElementById('tabelBody').addEventListener('click', function (e) {
       if (e.target.classList.contains('btn-remove')) {
-          e.target.parentElement.parentElement.remove();
+        e.target.parentElement.parentElement.remove();
       }
     });    
-    </script>      
-  </body>
+  </script>
+</body>
+
 </html>
--- index.php
+++ index.php
@@ -1,69 +1,66 @@
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Login Area</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-  </head>
-  <body style="background-color:black;">
+    <style>
+        body {
+            background-image: url('https://i.pinimg.com/564x/7b/a6/f2/7ba6f2ce9a07d38dc17dd40b232689ac.jpg');
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-attachment: fixed;
+        }
+    </style>
+</head>
+
+<body>
     <div class="container">
         <div class="row d-flex align-items-center justify-content-center" style="height: 100vh; width:100%">
             <div class="col-md-6">
                 <div class="card border-0 shadow rounded">
                     <div class="card-body">
-                        <div   div class="row">
+                        <div div class="row">
                             <form action="actionLogin.php" method="post" id="loginForm">
                                 <div class="card-header mb-3">
                                     <strong>Login</strong>
                                 </div>
-                                <input 
-                                    class="form-control"
-                                    type="text"
-                                    name="username"
-                                    placeholder="Username"
-                                    autofocus
+                                <input class="form-control" type="text" name="username" placeholder="Username" autofocus
                                     autocomplete="off">
-                                <input 
-                                    class="form-control mt-3"
-                                    type="password"
-                                    name="password"
-                                    placeholder="Password"
+                                <input class="form-control mt-3" type="password" name="password" placeholder="Password"
                                     autocomplete="off">
-                                <button type="submit" class="btn btn-primary mt-3"><i class="bi bi-box-arrow-in-right"></i> Login</button>
-                                <a href="#" class="btn btn-outline-secondary mt-3" onclick="switchForms('registerForm')">Register</a>
+                                <button type="submit" class="btn btn-primary mt-3"><i
+                                        class="bi bi-box-arrow-in-right"></i> Login</button>
+                                <a href="#" class="btn btn-outline-secondary mt-3"
+                                    onclick="switchForms('registerForm')">Register</a>
                             </form>
-                        <form action="registrasi.php" method="post" id="registerForm" style="display: none;">
-                            <div class="card-header mb-3">
-                                <strong>Registrasi Akun Baru</strong>
-                            </div>
-                            <input 
-                                class="form-control"
-                                type="text"
-                                id="regUsername"
-                                name="reg_username"
-                                placeholder="Username register"
-                                autofocus
-                                autocomplete="off">
-                            <input 
-                                class="form-control mt-3"
-                                type="password"
-                                id="regPassword"
-                                name="reg_password"
-                                placeholder="Password register"
-                                autofocus
-                                autocomplete="off">
-                            <button type="submit" class="btn btn-primary mt-3" onclick="showAlert()">Register</button>
-                            <a href="#" class="btn btn-outline-danger mt-3" onclick="switchForms('loginForm')">Cancel</a>
-                        </form>
+                            <form action="registrasi.php" method="post" id="registerForm" style="display: none;">
+                                <div class="card-header mb-3">
+                                    <strong>Registrasi Akun Baru</strong>
+                                </div>
+                                <input class="form-control" type="text" id="regUsername" name="reg_username"
+                                    placeholder="Username register" autofocus autocomplete="off">
+                                <input class="form-control mt-3" type="password" id="regPassword" name="reg_password"
+                                    placeholder="Password register" autofocus autocomplete="off">
+                                <button type="submit" class="btn btn-primary mt-3"
+                                    onclick="showAlert()">Register</button>
+                                <a href="#" class="btn btn-outline-danger mt-3"
+                                    onclick="switchForms('loginForm')">Cancel</a>
+                            </form>
                         </div>
                     </div>
-                  </div>
+                </div>
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+        crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <script>
@@ -81,20 +78,21 @@
         }
     </script>
     <script>
-        function showAlert(){
+        function showAlert() {
             swal.fire({
                 title: 'Registrasi Berhasil',
                 text: 'Anda telah berhasil mendaftar',
                 icon: 'success',
                 confirmButtonText: 'Ok',
             }).then((result) => {
-               if(result.isConfirmed){
-                window.location.href = 'index.php';
-               }     
+                if (result.isConfirmed) {
+                    window.location.href = 'index.php';
+                }
             }).finally(() => {
                 switchForms('#loginForm');
             });
         }
     </script>
-  </body>
+</body>
+
 </html>
